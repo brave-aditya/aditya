cbf9c8daea39cdaffc303470237ad8bf5dbb8426
aditya pratap <pratapaditya84@gmail.com>
2023-10-10 09:34:22 +0530

    lk

--- src/app/components/Quotes.jsx
+++ src/app/components/Quotes.jsx
@@ -1,18 +1,57 @@
-
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const Quotes = () => {
-  return (
-    <div className='py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <div className="mx-auto pb-1 w-fit bg-gradient-to-br from-[#ff0039] via-[#fc00ff] to-[#ff0039] mb-4">
-            <span className="block top-0 bg-[#121212] text-4xl font-bold text-white py-2 px-2">Quote of the Day</span>
-          </div>
-        <div className='sm:border-[#33353F] sm:border rounded-md py-8 px-4 text-white'>
-             <p className='text-center text-xl'>'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.'</p>
-            <p className='text-center font-bold text-xl mt-2'> – Ralph Waldo Emerson.</p>
-        </div>
-    </div>
-  )
-}
+  const [quote, setQuote] = useState(null);
 
-export default Quotes
+  useEffect(() => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((response) => {
+        setQuote(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="mx-auto pb-1 w-fit bg-gradient-to-br from-[#ff0039] via-[#fc00ff] to-[#ff0039] mb-4">
+        <span className="block top-0 bg-[#121212] text-4xl font-bold text-white py-2 px-2">
+          Quote of the Day
+        </span>
+      </div>
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-4 text-white">
+        {quote ? (
+          <div>
+            <p className="text-center text-xl">
+               <TypeAnimation
+              sequence={[
+                `'${quote.content}'`,
+                1000,
+              ]}
+              wrapper="span"
+              cursor={false}
+              speed={50}
+              repeat={Infinity}
+            /></p>
+            <p className="text-center font-bold text-xl mt-2">
+            <TypeAnimation
+              sequence={[
+                `- ${quote.author}`,
+                1000,
+              ]}
+              wrapper="span"
+              cursor={false}
+              speed={50}
+              repeat={Infinity}
+            />
+            </p>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Quotes;
